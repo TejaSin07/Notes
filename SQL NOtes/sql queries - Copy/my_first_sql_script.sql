7fff22ca988c52041c044e902637e1aad68c9941
tejas <tejassinkar10@gmail.com>
2026-01-21 23:02:09 +0530

    pushing sql notes

---
+++ SQL NOtes/sql queries - Copy/my_first_sql_script.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS startersql;
+USE startersql;
+
+select* from users;
---
+++ SQL NOtes/sql queries - Copy/second.sql
@@ -0,0 +1,9 @@
+use sbms39;
+show tables;
+
+
+drop table adrss_tbl;
+select * from emp_tbl;
+select * from addr_tbl;
+desc _tbl;
+desc passport_tbl;
